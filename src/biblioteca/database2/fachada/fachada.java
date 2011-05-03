c131bcc15864f4d033e5692e42c4c3ff6167ace2
guardo823 <guardo823@gmail.com>
2011-05-02 19:40:59 -0700

    Edited src/biblioteca/database2/fachada/fachada.java via GitHub

--- src/biblioteca/database2/fachada/fachada.java
+++ src/biblioteca/database2/fachada/fachada.java
@@ -4,10 +4,51 @@
  */
 package biblioteca.database2.fachada;
 
+package accesoDatos;
+
 /**
  *
- * @author alejandro
+ * @author Eduardo
  */
-public class fachada {
-    
+import java.sql.*;
+public class Fachada {
+
+    String url, usuario, password;
+        Connection conexion;
+        Statement instruccion;
+        ResultSet tabla;
+        Fachada(){
+            url="jdbc:postgresql://localhost:5432/bibliotecadigital";
+            usuario="guardo823";
+            password="guardo823";
+        }
+
+        public Connection conectar(){
+            try {
+            // Se carga el driver
+            Class.forName("org.postgresql.Driver");
+            System.out.println( "Driver Cargado" );
+            } catch( Exception e ) {
+                System.out.println( "No se pudo cargar el driver." );
+            }
+
+            try{
+                     //Crear el objeto de conexion a la base de datos
+                     conexion = DriverManager.getConnection(url, usuario, password);
+                     System.out.println( "Conexion Abierta" );
+                     return conexion;
+                  //Crear objeto Statement para realizar queries a la base de datos
+             } catch( Exception e ) {
+                System.out.println( "No se pudo abrir." );
+                return null;
+             }
+        }
+
+        public void cerrarConexion(Connection conect){
+            try{
+                 conect.close();
+            } catch( Exception e ) {
+                System.out.println( "No se pudo cerrar la BD" );
+            }
+        }
 }
